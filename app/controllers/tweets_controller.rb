34843ae07fe268a59f4ab60869a5448672271eb3
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 18:29:21 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -63,7 +63,7 @@ class TweetsController < ApplicationController
 
   end
 
-  delete '/tweets/:id' do
+  delete '/tweets/:id/delete' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet.user == current_user
