de6a2cb6eb38028a381df1c73d477eedb9a410c6
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 17:42:47 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    erb :login
+      if !logged_in 
+        erb :login
+      else
+        redirect '/tweets'
+      end
   end
 
   post '/signup' do
@@ -34,7 +38,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    if !logged_in 
+  
       user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
@@ -42,7 +46,6 @@ class ApplicationController < Sinatra::Base
       else
         redirect '/login'
       end
-    else
       
     end
   end
