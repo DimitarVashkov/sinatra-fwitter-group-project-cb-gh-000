543751ba90bbbbab4657e43a7fb3b694559080e3
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 17:12:18 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180407170511) do
+ActiveRecord::Schema.define(version: 20180407170517) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
