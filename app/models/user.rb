aee9405a34eec26df944c2e2991abd7ebd136df2
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 17:27:53 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
 has_many :tweets
 has_secure_password
+validate :username, presence: true
+validate :password, presence: true
+validate :email, presence: true
 
   def slug
     username.downcase.gsub(' ','-')
