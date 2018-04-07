be7cc121543c3eb136993b39b525ebb4ee96fdcd
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-07 18:19:56 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -41,7 +41,8 @@ class TweetsController < ApplicationController
   end
 
   get '/tweets/:id/edit' do
-    
+    @tweet = Tweet.find_by_id(params[:id])
+    erb :'tweets/edit'
   end
 
   patch '/tweets/:id' do
